d87149bd7a9d2a86d7d6f13652329631430dceef
Javier Caro <jcaro1993@gmail.com>
2021-01-11 09:17:35 -0800

    added new routing

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const router = express.Router()
-const Workout = require("../models/index")
+const Workout = require("../models/workout.js")
 
 
 router.get("/", (req, res) => {
@@ -20,10 +20,61 @@ router.get("/exercise", (req, res) => {
 
 router.get("/api/workouts", (req, res) => {
     Workout.find()
-    .then(workouts => {
-        res.json(workouts)
+        .then(workouts => {
+            res.json(workouts)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(workouts => {
+            res.json(workouts)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+router.get("/api/workouts/range", (req, res) => {
+    // aggregating code https://masteringjs.io/tutorials/mongoose/aggregate
+    Workout.aggregate([
+        {
+            // aggregating the fields based off of total duration which is equal to the sum of the exercise duration
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+        // sorting the workout results by id and then only returning 7 results (7 days)
+        .sort({ _id: 1 })
+        .limit(7)
+        .then(results => {
+            res.json(results)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+
+    //finding by id and updating https://kb.objectrocket.com/mongo-db/how-to-use-mongoose-to-find-by-id-and-update-with-an-example-1209
+    Workout.findByIdAndUpdate(params.id ,{ exercises: body}, function(err, result){
+
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send(result)
+        }
+
     })
-} )
 
-
+})
 module.exports = router
--- seeders/seed.js
+++ seeders/seed.js
@@ -3,7 +3,7 @@ let db = require("../models/index.js");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 let workoutSeed = [
